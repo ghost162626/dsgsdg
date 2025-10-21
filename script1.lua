eea8bec4f8cd0271c50bb96c0b79a4844922f15f
ghost162626 <ghost123456789kk@gmail.com>
2025-10-20 21:14:40 -0300

    Create script1.lua

---
+++ script1.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ghost162626/dsgsdg/refs/heads/main/script.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ghost162626/dsgsdg/refs/heads/main/battlegrounds1.lua"))() 
